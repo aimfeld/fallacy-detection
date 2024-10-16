3a5579f39662109a1d595449682d741668edabfa
Adrian Imfeld <aimfeld80@gmail.com>
2024-10-16 14:52:23 +0200

    use enum for LLM labels

--- src/llms.py
+++ src/llms.py
@@ -7,14 +7,17 @@ from langchain_google_vertexai import ChatVertexAI
 from langchain_community.llms import HuggingFaceEndpoint
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from enum import Enum
 
-LLM_GPT_4 = "gpt_4"
-LLM_GPT_4O = "gpt_4o"
-LLM_GPT_4O_MINI = "gpt_4o_mini"
-LLM_GPT_O1_MINI = "gpt_o1_mini"
-LLM_SONNET = "sonnet_3_5"
-LLM_GEMINI = "gemini_1_5_pro"
-LLM_LLAMA = "llama_3_8b"
+class LLMLabels(Enum):
+    GPT_4 = "gpt_4"
+    GPT_4O = "gpt_4o"
+    GPT_4O_MINI = "gpt_4o_mini"
+    GPT_O1_MINI = "gpt_o1_mini"
+    SONNET = "sonnet_3_5"
+    GEMINI = "gemini_1_5_pro"
+    LLAMA = "llama_3_8b"
+
 
 # Initialize LangChain
 def init_langchain():
@@ -25,17 +28,17 @@ def init_langchain():
     os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 # Type definitions
-LLMs = dict[str, Runnable]
+LLMs = dict[LLMLabels, Runnable]
 
 
 # Get the LLMs
-def get_llms(llm_names: list[str]) -> LLMs:
+def get_llms(llm_names: list[LLMLabels]) -> LLMs:
     llms: LLMs = {}
 
     # OpenAI models: https://platform.openai.com/docs/models
-    if LLM_GPT_4 in llm_names:
+    if LLMLabels.GPT_4 in llm_names:
         # https://platform.openai.com/docs/models
-        llms[LLM_GPT_4] = ChatOpenAI(
+        llms[LLMLabels.GPT_4] = ChatOpenAI(
             openai_api_key=os.getenv("OPENAI_API_KEY"),
             model="gpt-4-0613",
             temperature=0,
@@ -43,8 +46,8 @@ def get_llms(llm_names: list[str]) -> LLMs:
             max_retries=3,
         )
 
-    if LLM_GPT_4O in llm_names:
-        llms[LLM_GPT_4O] = ChatOpenAI(
+    if LLMLabels.GPT_4O in llm_names:
+        llms[LLMLabels.GPT_4O] = ChatOpenAI(
             openai_api_key=os.getenv("OPENAI_API_KEY"),
             model="gpt-4o-2024-08-06",
             temperature=0,
@@ -52,8 +55,8 @@ def get_llms(llm_names: list[str]) -> LLMs:
             max_retries=3,
         )
 
-    if LLM_GPT_4O_MINI in llm_names:
-        llms[LLM_GPT_4O_MINI] = ChatOpenAI(
+    if LLMLabels.GPT_4O_MINI in llm_names:
+        llms[LLMLabels.GPT_4O_MINI] = ChatOpenAI(
             openai_api_key=os.getenv("OPENAI_API_KEY"),
             model="gpt-4o-mini-2024-07-18",
             temperature=0,
@@ -61,8 +64,8 @@ def get_llms(llm_names: list[str]) -> LLMs:
             max_retries=3,
         )
 
-    if LLM_GPT_O1_MINI in llm_names:
-        llms[LLM_GPT_O1_MINI] = ChatOpenAI(
+    if LLMLabels.GPT_O1_MINI in llm_names:
+        llms[LLMLabels.GPT_O1_MINI] = ChatOpenAI(
             openai_api_key=os.getenv("OPENAI_API_KEY"),
             model="gpt-o1-mini-2024-09-12",
             temperature=0,
@@ -70,9 +73,9 @@ def get_llms(llm_names: list[str]) -> LLMs:
             max_retries=3,
         )
 
-    if LLM_SONNET in llm_names:
+    if LLMLabels.SONNET in llm_names:
         # https://python.langchain.com/docs/integrations/platforms/anthropic/
-        llms[LLM_SONNET] = ChatAnthropic(
+        llms[LLMLabels.SONNET] = ChatAnthropic(
             api_key=os.getenv("ANTHROPIC_API_KEY"),
             model="claude-3-5-sonnet-20240620",
             temperature=0,
@@ -80,9 +83,9 @@ def get_llms(llm_names: list[str]) -> LLMs:
             max_retries=3,
         )
 
-    if LLM_GEMINI in llm_names:
+    if LLMLabels.GEMINI in llm_names:
         # https://python.langchain.com/docs/integrations/chat/google_vertex_ai_palm/
-        llms[LLM_GEMINI] = ChatVertexAI(
+        llms[LLMLabels.GEMINI] = ChatVertexAI(
             api_key=os.getenv("GOOGLE_APPLICATION_CREDENTIALS"),
             model="gemini-1.5-pro",
             temperature=0,
@@ -90,7 +93,7 @@ def get_llms(llm_names: list[str]) -> LLMs:
             max_retries=3,
         )
 
-    if LLM_LLAMA in llm_names:
+    if LLMLabels.LLAMA in llm_names:
         hugging_face_endpoint = HuggingFaceEndpoint(
             # repo_id="meta-llama/Meta-Llama-3.1-405B-Instruct",
             # repo_id="meta-llama/Meta-Llama-3.1-8B-Instruct",
@@ -102,6 +105,6 @@ def get_llms(llm_names: list[str]) -> LLMs:
             input_variables=["question"],
             template="Question: {question}\n\nAnswer:"
         )
-        llms[LLM_LLAMA] = LLMChain(llm=hugging_face_endpoint, prompt=prompt_template)
+        llms[LLMLabels.LLAMA] = LLMChain(llm=hugging_face_endpoint, prompt=prompt_template)
 
     return llms
