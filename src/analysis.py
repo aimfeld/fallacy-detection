7aa3b88e7a7ffef9a92aab30a1b50136649652f1
Adrian Imfeld <aimfeld80@gmail.com>
2024-10-17 12:57:49 +0200

    analysis

--- src/analysis.py
+++ src/analysis.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
-from .llms import LLMLabel
+from .llms import LLM
 
 
 def score_fallacy_identification(df_fallacies: pd.DataFrame):
-    for llm_label in LLMLabel:
+    for llm_label in LLM:
         response_column = f"{llm_label.value}_response"
         if response_column not in df_fallacies.columns:
             continue
@@ -13,6 +13,16 @@ def score_fallacy_identification(df_fallacies: pd.DataFrame):
         df_fallacies[score_column] = df_fallacies.apply(
             lambda row: _get_fallacy_identification_score(row["label"], row[response_column]), axis=1
         ).astype('Int64')
+
+
+def get_fallacy_identification_scores(df_fallacies: pd.DataFrame) -> pd.DataFrame:
+    llm_scores = {}
+    for llm in LLM:
+        score_column = llm.value + '_score'
+        if score_column in df_fallacies.columns:
+            llm_scores[llm.label] = np.round(df_fallacies[score_column].mean() * 100, 1)
+
+    return pd.DataFrame(llm_scores.items(), columns=['LLM', 'Score'])
 
 
 def _get_fallacy_identification_score(label: int, response: str):
