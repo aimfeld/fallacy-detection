6ab5b25a14002f03b71a92097ccde675fb844679
Adrian Imfeld <aimfeld80@gmail.com>
2024-10-17 17:28:43 +0200

    grouped accuracies

--- src/analysis.py
+++ src/analysis.py
@@ -15,16 +15,6 @@ def score_fallacy_identification(df_fallacies: pd.DataFrame):
         ).astype('Int64')
 
 
-def get_fallacy_identification_scores(df_fallacies: pd.DataFrame) -> pd.DataFrame:
-    llm_scores = {}
-    for llm in LLM:
-        score_column = llm.value + '_score'
-        if score_column in df_fallacies.columns:
-            llm_scores[llm.label] = np.round(df_fallacies[score_column].mean() * 100, 1)
-
-    return pd.DataFrame(llm_scores.items(), columns=['LLM', 'Score'])
-
-
 def _get_fallacy_identification_score(label: int, response: str):
     filtered_response = response.lower()
     if "no" in filtered_response:
@@ -35,3 +25,25 @@ def _get_fallacy_identification_score(label: int, response: str):
         return pd.NA
 
     return 1 if response_label == label else 0
+
+
+def get_fallacy_identification_accuracies(df_fallacies: pd.DataFrame) -> pd.DataFrame:
+    accuracies = {}
+    for llm in LLM:
+        score_column = llm.value + '_score'
+        if score_column in df_fallacies.columns:
+            accuracies[llm.label] = np.round(df_fallacies[score_column].mean() * 100, 1)
+
+    return pd.DataFrame(accuracies.items(), columns=['LLM', 'Accuracy'])
+
+
+def get_fallacy_identification_grouped_accuracies(df_fallacies: pd.DataFrame, groupby: list[str]):
+    accuracies = {}
+    for llm in LLM:
+        score_column = llm.value + '_score'
+        if score_column in df_fallacies.columns:
+            accuracies[llm.label] = np.round(df_fallacies.groupby(groupby)[score_column].mean() * 100, 1)
+
+    return pd.DataFrame.from_dict(accuracies, orient='index')
+
+
