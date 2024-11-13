ada3dfa53d22e0a013e0ee799b8234fabf0951d7
Adrian Imfeld <aimfeld80@gmail.com>
2024-11-13 11:19:47 +0100

    use csv format

--- src/mafalda.py
+++ src/mafalda.py
@@ -4,34 +4,41 @@ This module functions for dealing with the MAFALDA dataset by Helwe et al. (2024
 import pandas as pd
 import json
 from .utils import log
-import os.path
 
+# Columns that contain dictionaries that need to be converted to/from JSON
+DICT_COLS = ['sentences_with_labels']
 
 def create_mafalda_df() -> pd.DataFrame:
     df = pd.read_json('datasets/MAFALDA/gold_standard_dataset.jsonl', lines=True)
-    df['sentences_with_labels'] = df['sentences_with_labels'].apply(json.loads)
 
     return df
 
 
 def get_mafalda_df(filename: str) -> pd.DataFrame:
     """
-    Load the mafalda dataframe from a JSONL file, or create a new one if the file doesn't exist.
+    Load the mafalda dataframe from a CSV file, or create a new one if the file doesn't exist.
     """
-    if os.path.isfile(filename):
-        df = pd.read_json(filename, lines=True)
+    try:
+        df = pd.read_csv(filename)
 
         log(f"Loaded existing mafalda dataframe from {filename}.")
-    else:
+    except FileNotFoundError:
         df = create_mafalda_df()
 
         log("Created new mafalda dataframe.")
+
+    for col in DICT_COLS:
+        df[col] = df[col].apply(json.loads)
 
     return df
 
 
 def save_mafalda_df(df: pd.DataFrame, filename: str):
     """
-    Save the mafalda dataframe to a JSON file.
+    Save the mafalda dataframe to a CSV file.
     """
-    df.to_json(filename, index=False, orient='records', lines=True)
+    df = df.copy()
+    for col in DICT_COLS:
+        df[col] = df[col].apply(json.dumps)
+
+    df.to_csv(filename, index=False)
